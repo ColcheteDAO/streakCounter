6b9fa68a56a6d01d6ab0c7ec2161692a984d829d
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 21:54:52 -0300

    Update calculateStreak.sh

--- calculateStreak.sh
+++ calculateStreak.sh
@@ -13,8 +13,8 @@ YEARS_TO_RUN=$((TODAY_YEAR - ACCOUNT_CREATED_AT_YEAR))
 echo "acccvre"
 echo "$YEARS_TO_RUN"
 RUN_YEAR=$ACCOUNT_CREATED_AT_YEAR
-touch "$USERNAMEContributions.json"
-echo "[]" > "$USERNAMEContributions.json"
+touch "${USERNAME}Contributions.json"
+echo "[]" > "${USERNAME}Contributions.json"
 for i in $(seq 0 $YEARS_TO_RUN)
 do
     EVENTS=$(curl --location 'https://api.github.com/graphql' \
@@ -23,10 +23,10 @@ do
     --data '{
         "query": "query { user(login: \"juancolchete\") { name createdAt contributionsCollection(from: \"'$RUN_YEAR'-01-01T00:00:00Z\") { startedAt contributionCalendar { totalContributions weeks { contributionDays { date contributionCount } } } } } }"
     }')
-    echo $EVENTS | jq '[.data.user.contributionsCollection.contributionCalendar.weeks[].contributionDays[]] | .[:-1]' > "$USERNAMEContributions.json"
+    echo $EVENTS | jq '[.data.user.contributionsCollection.contributionCalendar.weeks[].contributionDays[]] | .[:-1]' > "${USERNAME}Contributions.json"
     echo $USERNAME started at $ACCOUNT_CREATED_AT streak 9000
     RUN_YEAR=$((ACCOUNT_CREATED_AT_YEAR + 1))
     
     jq -n --argjson old "$(cat ${USERNAME}Contributions.json)" --argjson new "$EVENTS" '$old + $new' > "temp.json" && mv "temp.json" "${USERNAME}Contributions.json"
 done
-cat "$USERNAMEContributions.json"
+cat "${USERNAME}Contributions.json"

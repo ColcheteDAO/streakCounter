707a09ed20b8254fde4c316a9866b1d7c6e43a49
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-19 22:43:22 -0300

    ci: implement user streak calculation

--- calculateUserStreak.sh
+++ calculateUserStreak.sh
@@ -1,1 +1,12 @@
+USERNAME=$1
+STREAK_COUNT=0
+cat "contributions/${USERNAME}.json" | jq '.[].contributionCount' | while read -r count; 
+do 
+  if [[ $count -gt 0 ]]; then
+    STREAK_COUNT=$(( STREAK_COUNT + 1 ))
+  else
+    STREAK_COUNT=0
+  fi
+done
 
+echo $USERNAME streak is $STREAK_COUNT

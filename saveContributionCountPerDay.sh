7ccd7c384b3a8e512bacf81a9c4d9c23e839e6f5
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-20 14:53:02 -0300

    chore: access user data

--- saveContributionCountPerDay.sh
+++ saveContributionCountPerDay.sh
@@ -9,11 +9,12 @@ EVENTS=$(curl -S --location 'https://api.github.com/graphql' \
 TODAY_YEAR=$(date -u +"%Y")
 ACCOUNT_CREATED_AT=$(echo $EVENTS | jq -r '.data.user.createdAt')
 ACCOUNT_CREATED_AT_YEAR=${ACCOUNT_CREATED_AT:0:4}
+USER_DATA=$(echo $EVENTS | jq '.data')
 RUN_YEAR=$ACCOUNT_CREATED_AT_YEAR
 mkdir -p contributions
 mkdir -p data
 echo "[]" > "contributions/${USERNAME}.json"
-echo $EVENTS > "data/${USERNAME}.json"
+echo $USER_DATA > "data/${USERNAME}.json"
 
 for i in $(seq $ACCOUNT_CREATED_AT_YEAR $TODAY_YEAR)
 do

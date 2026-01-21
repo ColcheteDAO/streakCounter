d34476ba4997927eb1acca894b61cb98ad6ca2f1
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-21 07:24:42 -0300

    feat: calculate current streak date

--- calculateUserStreak.sh
+++ calculateUserStreak.sh
@@ -7,16 +7,21 @@ CONTRIBUTION_DAYS_COUNT=$(cat "contributions/${USERNAME}.json" | jq -r '[.[] | s
 MAX_STREAK=0
 INDEX=0
 MAX_STREAK_DATE=""
+CURRENT_STREAK_DATE=""
 while read -r count; 
 do 
   if [[ $count -gt 0 ]]; then
     STREAK_COUNT=$(( STREAK_COUNT + 1 ))
+    if [[ $STREAK_COUNT -eq 1 ]]; then
+      CURRENT_STREAK_DATE=$(cat "contributions/${USERNAME}.json" | jq -r '.['$INDEX'].date')
+    fi
     CONTRIBUTION_COUNT=$(( CONTRIBUTION_COUNT + count ))
     if [[ $STREAK_COUNT -gt $MAX_STREAK ]]; then
       MAX_STREAK=$STREAK_COUNT
       MAX_STREAK_DATE=$(cat "contributions/${USERNAME}.json" | jq -r '.['$(( $INDEX - $MAX_STREAK))'].date')
     fi
   else
+    CURRENT_STREAK_DATE=$(cat "contributions/${USERNAME}.json" | jq -r '.['$INDEX'].date')
     STREAK_COUNT=0
   fi
   INDEX=$(( $INDEX + 1 ))
@@ -33,6 +38,7 @@ cat >"streakData/${USERNAME}.json" <<EOL
   "contributionCount": "$CONTRIBUTION_COUNT",
   "avgContribution": "$AVG_CONTRIBUTION",
   "maxStreak": "$MAX_STREAK",
-  "maxStreakDate": "$MAX_STREAK_DATE"
+  "maxStreakDate": "$MAX_STREAK_DATE",
+  "currentStreakDate": "$CURRENT_STREAK_DATE"
 }
 EOL

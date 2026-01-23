d859f9ca0f0e58c3a607c6fb12a99e542104e3e3
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2026-01-22 21:56:22 -0300

    ci: remove debug here

--- statistics.sh
+++ statistics.sh
@@ -11,7 +11,6 @@ DAY_COMMITMENT_DATA+="["
 WEEK_NUMBER+="["
 while read -r contribution; 
 do 
-  echo here
   CONTRIBUTION_COUNT=$(echo $contribution | jq -r ".contributionCount")
   CONTRIBUTION_DATE=$(echo $contribution | jq -r ".date")
   CONTRIBUTION_YEAR=${CONTRIBUTION_DATE:0:4}
